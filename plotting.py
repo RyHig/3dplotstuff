7c5ec6fe86ab4101e443b7f06e443f9bd2e6911e
Ryan Higgins <ryanjeffhiggins@gmail.com>
2019-07-22 10:26:02 -0500

    Cleaned up the spiral_3d function and found a way to iterate over N frames to animate

--- plotting.py
+++ plotting.py
@@ -1,38 +1,34 @@
 import plotly.graph_objects as go
 import numpy as np
 
+N = 100
+
 
 def bar_2d():
     fig = go.Figure(
         data=[go.Bar(y=[2, 1, 3])],
         layout_title_text="A Figure Displayed with fig.show()"
     )
-
     fig.show()
 
 
-def make_values(start, stop, pts):
-    t = np.linspace(start, stop, pts)
+def make_values(start):
+    t = np.linspace(start, start + 10, 20)
     v = (np.cos(t), np.sin(t), t)
     return v
 
 
-def spiral_3d():
-    v = []
-    for i in range(5):
-        v.append(make_values(5*i, 5*i+10, 50))
-
+def spiral_3d(values):
     fig = go.Figure(
         data=[go.Scatter3d(
-            x=v[0][0],
-            y=v[0][1],
-            z=v[0][2],
+            x=values[0],
+            y=values[1],
+            z=values[2],
             mode='markers',
-            marker=dict(
-                size=12,
-                color=v[0][2],
-                colorscale='Viridis',
-                opacity=0.8)
+            marker=dict(size=12,
+                        color=v[0][2],
+                        colorscale='Viridis',
+                        opacity=0.8)
             )],
         layout=go.Layout(
             margin=dict(l=0, r=0, b=0, t=0),
@@ -42,13 +38,13 @@ def spiral_3d():
                          method="animate",
                          args=[None])])]
         ),
-        frames=[go.Frame(data=[go.Scatter3d(x=v[1][0], y=v[1][1], z=v[1][2])]),
-                go.Frame(data=[go.Scatter3d(x=v[2][0], y=v[2][1], z=v[2][2])]),
-                go.Frame(data=[go.Scatter3d(x=v[3][0], y=v[3][1], z=v[3][2])]),
-                go.Frame(data=[go.Scatter3d(x=v[4][0], y=v[4][1], z=v[4][2])])]
+        frames=[go.Frame(
+            data=[go.Scatter3d(x=k[0], y=k[1], z=k[2])]) for k in v]
     )
 
     fig.show()
 
 
-spiral_3d()
+if __name__ == "__main__":
+    v = list(map(make_values, range(N)))
+    spiral_3d(v)
